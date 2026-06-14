061b5cf790b90f65456afff1b48946b4ac0ee5f0
Abhishek kudiya <abhishekkudiya85@gmail.com>
2026-06-14 20:39:55 +0530

    Implement detectCapitalUse function in Solution class

---
+++ 520-Detect-Capital.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+
+        int capitalCount = 0;
+
+        for (char ch : word) {
+            if (isupper(ch))
+                capitalCount++;
+        }
+
+        return capitalCount == word.length() ||
+               capitalCount == 0 ||
+               (capitalCount == 1 && isupper(word[0]));
+    }
+};
